685ab54c3bce8c3f471bf7ee445e03c6bee566c2
Amune victor <viktorefi2001@gmail.com>
2025-01-28 14:49:36 +0100

    pushing changes

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,7 +19,21 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+let analytics;
+isSupported()
+  .then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+      console.log("Firebase Analytics initialized");
+    } else {
+      console.log("Firebase Analytics is not supported in this environment");
+    }
+  })
+  .catch((error) => {
+    console.error("Error checking analytics support:", error);
+  });
+
 
 const db = getFirestore(app);
 
